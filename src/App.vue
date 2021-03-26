91dee9873ef3eb52c16c0e34ab2fe5fad64884ab
Xheghun <ysenruf@gmail.com>
2021-03-26 19:24:01 +0400

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
-   <div class="container">
-     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-       <ul class="navbar-nav">
-         <li class="nav-item">
-         </li>
-       </ul>
-     </nav>
-    <router-view></router-view>
+  <div class="container">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/create" class="nav-link">Create Post</router-link>
+        </li>
+
+        <li class="nav-item">
+          <router-link to="/posts" class="nav-link">Post</router-link>
+        </li>
+      </ul>
+    </nav>
+    <br />
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-
-export default {
-};
+export default {};
 </script>
 
 <style>
@@ -26,11 +35,13 @@ HomeComponent #app {
   margin-top: 60px;
 }
 
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
 
-.fade-enter, .fade-leave-active {
+.fade-enter,
+.fade-leave-active {
   opacity: 0;
 }
 </style>
