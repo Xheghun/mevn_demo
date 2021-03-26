454dc39e932a6b2ec909031f77cbe105896dd498
Xheghun <ysenruf@gmail.com>
2021-03-26 19:21:13 +0400

    init routes

---
+++ src/components/EditComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Edit Component</div>
+
+                <div class="card-body">
+                    I'm the Edit Component you know
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,47 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import HomeComponent from './components/HomeComponent.vue';
+import CreateComponent from './components/CreateComponent.vue';
+import EditComponent from './components/EditComponent.vue';
+import IndexComponent from './components/IndexComponent.vue';
+
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: HomeComponent
+  },
+
+  {
+    name: 'create',
+    path: '/create',
+    component: CreateComponent
+  },
+
+  {
+    name: 'edit',
+    path: '/edit',
+    component: EditComponent
+  },
+
+  {
+    name: 'post',
+    path: '/posts',
+    component: IndexComponent
+  },
+
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  router: routes
+});
+
+new Vue(Vue.util.extend({router}),App).$mount('#app')
